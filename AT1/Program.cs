82b57ce0ccf0e40f31bdaf7068a15c054a70911e
KenanNagiyev <788lnmp@code.edu.az>
2024-05-01 23:12:41 +0400

    first commit

---
+++ AT1/Program.cs
@@ -0,0 +1,17 @@
+﻿try
+{
+    Console.WriteLine("First Number:");
+    int a = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Second Number:");
+    int b = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Third Number:");
+    int c = Convert.ToInt32(Console.ReadLine());
+
+    int result = a + b + c;
+    Console.WriteLine("Result = " + result);
+}
+catch (Exception)
+{
+    Console.WriteLine("Please try again");
+
+}
